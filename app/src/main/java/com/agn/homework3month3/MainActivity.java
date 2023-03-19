716c7d3b85933a4df6f896c800e5aba1e5eb4a82
Argen Bagylov <121685247+AGNARE@users.noreply.github.com>
2023-03-19 16:01:15 +0600

    commit correcting

--- app/src/main/java/com/agn/homework3month3/MainActivity.java
+++ app/src/main/java/com/agn/homework3month3/MainActivity.java
@@ -5,36 +5,32 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.agn.homework3month3.reciclerView.Resturant;
 import com.agn.homework3month3.reciclerView.ResturentAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList <String> arrayList;
-    ResturentAdapter adapter;
-    RecyclerView recyclerView;
+    ArrayList<Resturant> list = new ArrayList<Resturant>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initList();
+        RecyclerView recyclerView = findViewById(R.id.recycleView);
+        ResturentAdapter adapter = new ResturentAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+        initData();
     }
 
-    private void initList() {
-        arrayList = new ArrayList<>();
-        arrayList.add("Plov");
-        arrayList.add("Lagman");
-        arrayList.add("Kurdak");
-        arrayList.add("Borsh");
-        arrayList.add("Sup");
-        arrayList.add("Manty");
-        arrayList.add("BestManty");
-        arrayList.add("BeshBarmak");
-        adapter = new ResturentAdapter(arrayList);
-
-        recyclerView = findViewById(R.id.recycleView);
-        recyclerView.setAdapter(adapter);
+    private void initData() {
+        list.add(new Resturant("Плов", "мясо, рис", "130som"));
+        list.add(new Resturant("Лагман", "мясо, лапша", "160som"));
+        list.add(new Resturant("Борщь", "мясо, капуста", "120som"));
+        list.add(new Resturant("Мясо", "мясо, картошка", "250som"));
+        list.add(new Resturant("Суп", "мясо, гречка", "80som"));
+        list.add(new Resturant("Манты", "тесто, мясо , картошка", "170som"));
     }
 }
--- app/src/main/java/com/agn/homework3month3/reciclerView/ResturentAdapter.java
+++ app/src/main/java/com/agn/homework3month3/reciclerView/ResturentAdapter.java
@@ -1,7 +1,10 @@
 package com.agn.homework3month3.reciclerView;
 
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,30 +12,46 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.agn.homework3month3.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ResturentAdapter extends RecyclerView.Adapter<ResturantsViewHolder> {
+public class ResturentAdapter extends RecyclerView.Adapter<ResturentAdapter.ViewHolder> {
+    private List<Resturant> list;
+    private LayoutInflater inflater;
 
-    ArrayList<String>arrayListFirst;
-
-    public ResturentAdapter(ArrayList<String> arrayList){
-        this.arrayListFirst = arrayList;
+    public ResturentAdapter(Context context, List<Resturant> list){
+        this.list = list;
+        this.inflater = LayoutInflater.from(context);
     }
 
     @NonNull
     @Override
-    public ResturantsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ResturantsViewHolder(LayoutInflater
-                .from(parent.getContext())
-                .inflate(R.layout.item_resturant,parent,false));
+    public ResturentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.item_resturant, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ResturantsViewHolder holder, int position) {
-        holder.onBind(arrayListFirst.get(position));
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Resturant list1 = list.get(position);
+        holder.name.setText(list1.getName());
+        holder.ingridinet.setText(list1.getIngridinet());
+        holder.rise.setText(list1.getRice());
     }
 
     @Override
     public int getItemCount() {
-        return arrayListFirst.size();
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView name;
+        TextView ingridinet;
+        TextView rise;
+        public ViewHolder(View view){
+            super(view);
+            name = view.findViewById(R.id.dishNameTextView);
+            ingridinet = view.findViewById(R.id.ingridientTextView);
+            rise = view.findViewById(R.id.priceTextView);
+        }
     }
 }
